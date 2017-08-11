9ca6eb24ffa26d2eea2ad9eb11889f7135655934
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-08-11 19:56:25 -0400

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational rat1 = new Rational(0, 1);
+            Rational rat2 = new Rational(1, 1);
+            Rational rat3 = new Rational(2, 3);
+            Rational rat4 = new Rational(3, 2);
+            Console.WriteLine("\t|-------------------------------------------- |");
+            Console.WriteLine("\t|The four original rational numbers are:      |");
+            Console.WriteLine("\t|-------------------------------------------- |");
+            Console.WriteLine("\t| {0} , {1} , {2},  {3}                           |", rat1,rat2,rat3,rat4);
+            rat2.IncreasedBy(rat3);
+            Console.WriteLine("\t|-------------------------------------------- |");
+            Console.WriteLine("\t|The sum of 1 and 2/3 is {0}                  |",rat2);
+            rat4.DecreasedBy(rat3);
+            Console.WriteLine("\t|-------------------------------------------- |");
+            Console.WriteLine("\t|The Diff of 3/2 and 2/3 is {0}               |", rat4);
+            Console.WriteLine("\t|-------------------------------------------- |");          
+            Console. ReadKey();
+        }
+    }
+}
---
+++ Assignment1/Rational.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Name: Bal Krishna Dhakal
+ * StudentID: 300916314
+ * Description: Assignment1
+ */ 
+/// <summary>
+/// This is my Assignment1
+/// </summary>
+namespace Assignment1
+{
+    class Rational
+    {
+        public int Denominator {get; private set;}
+        public int Numerator {get; private set;}
+        public Rational(int numerator=0, int denominator = 1)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+        public override string ToString()
+        {
+            if (Denominator != 1)
+            {
+                return string.Format("{0}/{1}", Numerator, Denominator);
+            }
+            else
+            {
+                return string.Format("{0}", Numerator);
+            }
+        }
+        public void IncreasedBy(Rational other)
+        {
+            Numerator = this.Numerator * other.Denominator + this.Denominator * other.Numerator;
+            Denominator = this.Denominator * other.Denominator;
+        }
+        public void DecreasedBy(Rational other)
+        {
+            Numerator = this.Numerator * other.Denominator - this.Denominator * other.Numerator;
+            Denominator = this.Denominator * other.Denominator;
+        }
+    }
+}
+
